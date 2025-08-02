e824d9fcf281fa307b49acf67b843b6682f4aacb
codylockyear <lilhoosier51@gmail.com>
2025-08-02 15:56:00 -0700

    Udpdate database connection to use DATABASE_URL for render

--- index.js
+++ index.js
@@ -45,7 +45,10 @@ for (const file of eventFiles) {
 }
 
 // Database Connection
-const sequelize = new Sequelize(process.env.PG_DATABASE, process.env.PG_USER, process.env.PG_PASSWORD, {
+const sequelize = new Sequelize(process.env.DATABASE_URL || {
+    database: process.env.PG_DATABASE,
+    username: process.env.PG_USER,
+    password: process.env.PG_PASSWORD,
     host: process.env.PG_HOST,
     dialect: 'postgres',
     logging: false, // Set to true for SQL query logging
